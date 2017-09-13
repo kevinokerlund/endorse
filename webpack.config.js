9eb15517280f0ff24526ed3cf94e6e210570d068
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2017-09-12 20:17:22 -0600

    npm and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/endorse.js',
+	output: {
+		filename: 'endorse.js',
+		path: path.resolve(__dirname, 'lib'),
+		library: "Endorse",
+		libraryTarget: "umd",
+		libraryExport: "default"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: /src/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			},
+		]
+	},
+};
